d5bbb7f2ec1be846bbf11610641cc496efe9725b
Cam-4587 Conroy <cameronconroy@live.com>
2024-10-15 16:59:04 +0000

    Transform category title into links

--- crashblog/blog/models.py
+++ crashblog/blog/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length = 255)
+    slug = models.SlugField(max_length = 255, unique=True)
+    
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+    
+    
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -11,6 +24,10 @@ class Post(models.Model):
     
     class Meta:
         ordering = ('-created_at',)
+    
+            
+    def __str__(self):
+        return self.title
         
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
